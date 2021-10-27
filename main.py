3b5eb2c401894b06b3a229c229467fb31fed29be
Jake Ahearne <84291763+jakedahearne@users.noreply.github.com>
2021-10-27 10:21:13 -0400

    Fixed Tier system and made TITLES and TIERS global

--- main.py
+++ main.py
@@ -3,8 +3,20 @@ import selenium
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
 
+TITLES = ["tier", "concrete restoration", "epoxy coatings", "floor coatings", "floor demo", "floor demolition",
+          "floor maintenance", "flooring maintenance", "floor restoration", "flooring restoration",
+          "surface preparation", "flooring", "surfacing", "restoration", "coating",
+          "construction", "polishing", "painting", "demolition", "epoxy", "concrete"]
 
-def count_words(df, t1, t234, titles):
+TIER_1 = ['concrete restoration', 'epoxy coatings', 'floor coatings', 'floor demo',
+          'floor demolition', 'floor maintenance', 'flooring maintenance', 'floor restoration',
+          'flooring restoration', 'surface preparation']
+
+TIER_234 = ["flooring", "surfacing", "restoration", "coating", "construction", "polishing",
+            "painting", "demolition", "epoxy", "concrete"]
+
+
+def count_words(df):
     count = 0
     whole = len(df)
     # Make sure you have geckodriver set up properly
@@ -15,31 +27,33 @@ def count_words(df, t1, t234, titles):
         website = df.loc[i, "Web Address (URL)"]
         website = "http://" + website.lower()
         tier = 0
+        tier_sum = 0
         try:
             browser.get(website)
             source = browser.find_element_by_tag_name("body").get_attribute("innerText")
             str_source = str(source).lower()
-            for word in t1:
+
+            for word in TIER_1:
                 df.at[i, word] = str_source.count(word)
                 if tier == 0 and str_source.count(word) > 0:
                     tier = 1
-            if str_source.count("concrete"):
-                for word in t234:
-                    df.at[i, "concrete AND " + word] = str_source.count(word)
-                    if tier == 0 and str_source.count(word) > 0:
-                        tier = 2
-            else:
-                for word in t234:
-                    df.at[i, word] = str_source.count(word)
-                    if tier == 0 and str_source.count(word) > 1:
-                        tier = 3
-            if tier == 0:
-                tier = 4
+            for word in TIER_234:
+                df.at[i, word] = str_source.count(word)
+                tier_sum += str_source.count(word)
+                if tier == 0 and tier_sum > 1 and str_source.count(word) > 1:
+                    tier = 2
+                elif tier == 0 and tier_sum > 1:
+                    tier = 3
+                else:
+                    tier = 4
             df.at[i, "tier"] = tier
         except selenium.common.exceptions.WebDriverException or TimeoutException:
             print("DNE")
-            for word in titles:
+            for word in TITLES:
                 df.at[i, word] = 0
+
+        if count % 1000 == 0:
+            df.to_csv("counted.csv")
 
         count += 1
         print("\n-------------------------")
@@ -48,29 +62,14 @@ def count_words(df, t1, t234, titles):
         print("-------------------------")
     browser.close()
 
-    return df
+    df.to_csv("counted.csv")
 
 
 def counter():
-    titles = ["tier", "concrete restoration", "epoxy coatings", "floot coatings", "floor demo", "floor demolition",
-              "floor maintenance", "flooring maintenance", "floor restoration", "flooring restoration",
-              "surface preparation", "concrete AND flooring", "concrete AND surfacing", "concrete AND restoration",
-              "concrete AND coating", "concrete AND construction", "concrete AND polishing", "concrete AND painting",
-              "concrete AND demolition", "concrete AND epoxy", "flooring", "surfacing", "restoration", "coating",
-              "construction", "polishing", "painting", "demolition", "epoxy"]
-
-    tier_1 = ['concrete restoration', 'epoxy coatings', 'floor coatings', 'floor demo',
-              'floor demolition', 'floor maintenance', 'flooring maintenance', 'floor restoration',
-              'flooring restoration', 'surface preparation']
-
-    tier_234 = ["flooring", "surfacing", "restoration", "coating", "construction", "polishing",
-                "painting", "demolition", "epoxy"]
 
     df = pd.read_csv("word_count.csv")
 
-    new_df = count_words(df, tier_1, tier_234, titles)
-
-    new_df.to_csv("counted.csv")
+    count_words(df)
 
 
 if __name__ == '__main__':
